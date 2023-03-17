a935e6f8d9f470bda820af0083e6d698d64c1f57
Fatehi Mohamed Alhabib <100781782+Simofatt@users.noreply.github.com>
2023-03-17 20:40:33 +0100

    Delete schoolManagementSystem directory

--- schoolManagementSystem/src/schoolManagementSystem/Main.java
+++
@@ -1,9 +0,0 @@
-package schoolManagementSystem;
-
-import java.security.NoSuchAlgorithmException;
-
-public class Main {
-	public static void main(String[] args) throws NoSuchAlgorithmException {
-	Menu.MenuPrincipal();
-	}
-}
